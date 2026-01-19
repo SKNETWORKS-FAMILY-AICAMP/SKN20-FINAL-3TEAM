bf7b7f8d4bf3ab665547da47134624a60d1cd23c
BBuSang <3179954@gmail.com>
2026-01-19 15:21:41 +0900

    로그인 로직 백엔드

---
+++ src/main/java/com/example/skn20/Skn20Application.java
@@ -0,0 +1,13 @@
+package com.example.skn20;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Skn20Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Skn20Application.class, args);
+    }
+
+}
